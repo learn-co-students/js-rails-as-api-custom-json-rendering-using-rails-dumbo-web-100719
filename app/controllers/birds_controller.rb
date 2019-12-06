e8b5f28e2daace4a08b26792b86bdc4348cd42dd
yuriyberezskyy <bereza95@gmail.com>
2019-12-06 12:00:09 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,14 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+
+  def show
+   @bird = Bird.find_by(id: params[:id])
+  if(@bird)
+    render json: @bird
+  else
+    render json: {message: 'No bird'}
+  end
+
+  end
 end
